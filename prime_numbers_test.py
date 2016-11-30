b644c1b82f25fbabf1040fcbac4b5d7c3143b4f1
ggiddy <nggitau@gmail.com>
2016-11-30 19:17:45 +0300

    New set of tests for generate_prime function

--- prime_numbers_test.py
+++ prime_numbers_test.py
@@ -16,11 +16,22 @@ class TestPrime(unittest.TestCase):
         The function should work with positive numbers
         """
         self.assertEqual(prime_generator(10), ['2', '3', '5', '7'])
+    
+    def test_rejects_negative_values(self):
+        """
+        The function should reject prime numbers
+        """
+        self.assertEqual(generate_prime(-10), "Please provide a positive value for an angument")
+
+    def test_rejects_numbers_less_than_1(self):
+        """
+        The function should reject numbers less than 1
+        """
+        self.assertEqual(generate_prime(0.8), "Please privide a number greater than or equal to 1")
 
     def test_returns_list(self):
         """
         The function returns an element of type list
         """
         self.assertEqual(isinstance(prime_generator(10)), isinstance(['1']))
-    
     
