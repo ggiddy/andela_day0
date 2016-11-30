507b094147001f9d69a8a8da0d20242907ea1d01
ggiddy <nggitau@gmail.com>
2016-11-30 19:33:55 +0300

    Implementing generate_prime method

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,4 +1,23 @@
 # Function to generate prime numbers from 0 to n with assymptotic analysis
 
 def generate_prime(n):
-    pass
+    """
+    Genetates prime numbers from 0 to n
+    """
+
+    prime_numbers = []
+
+    div = True
+
+    if n < 0:
+        print "Please provide a positive number"
+    else:
+        for i in range(2, n+1):
+            div = True
+
+            for j in range(2, i):
+                if i %j == 0:
+                    div = False
+            if div:
+                prime_numbers.append(i)
+        return prime_numbers
